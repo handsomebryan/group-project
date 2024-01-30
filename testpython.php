c73565efaa081a3226af4526ad47602bf2af2a00
Bryan <bryanh365@gmail.com>
2024-01-30 18:31:04 +0800

    Update testpython.php

--- testpython.php
+++ testpython.php
@@ -1,4 +1,6 @@
 <?php
 $script = __DIR__ . DIRECTORY_SEPARATOR . "function1.1.py";
 $result = shell_exec("python $script");
-echo "PHP got the result - $result";
+//echo "PHP got the result - $result";
+echo '<img src="/web/photo/image.png" alt="Generated Image">';
+?>
