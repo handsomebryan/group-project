c490866b8b9f4b2ace7997e65021ebaf4cc2d6d2
Bryan <bryanh365@gmail.com>
2024-03-26 11:24:09 +0800

    Update getSRelation.php

--- getSRelation.php
+++ getSRelation.php
@@ -8,66 +8,29 @@ function getQueryParam($paramName)
 
 $id = getQueryParam('id');
 
-// Query to select data from your database
-$sql = "SELECT DISTINCT
-RIGHT(業務員序號, 5) AS 業務員序號_last5,
-RIGHT(招攬業務員1序號, 5) AS 招攬業務員1序號_last5
-FROM 服務招攬業務員
-JOIN 保單資料 ON 保單資料.保單序號 = 服務招攬業務員.保單序號
-WHERE 服務招攬業務員.業務員序號 LIKE '%$id' AND 保單生效日 >= DATE_SUB(CURDATE(), INTERVAL 10 YEAR)";
+// SQL 查詢，計算服務招攬業務員1和招攬業務員2相同序號的保單資料的保險金額總和
+$sql = "SELECT RIGHT(業務員序號, 5) AS 業務員序號, SUM(年化保費) AS 總保費
+        FROM (
+            SELECT 業務員序號, 年化保費
+            FROM 保單資料 a
+            JOIN 服務招攬業務員 b ON a.保單序號 = b.保單序號
+            WHERE RIGHT(b.招攬業務員1序號, 5) LIKE '%$id'
+            UNION ALL
+            SELECT 業務員序號, 年化保費
+            FROM 保單資料 a
+            JOIN 服務招攬業務員 b ON a.保單序號 = b.保單序號
+            WHERE RIGHT(b.招攬業務員2序號, 5) LIKE '%$id'
+        ) AS sales
+        GROUP BY RIGHT(業務員序號, 5)
+        ORDER BY 總保費 DESC
+        LIMIT 5";
+
+$data = [];
 
 $result = $conn->query($sql);
-
-
-$graphData = [];
-
-
-
-if ($result->num_rows > 0) {
-    // Output data of each row
-    while ($row = $result->fetch_assoc()) {
-        $source = $row["業務員序號_last5"];
-        $target = $row["招攬業務員1序號_last5"];
-        
-
-        // Add edges to the graph data array
-        if (!isset($graphData[$source])) {
-            $graphData[$source] = [];
-        }
-        $graphData[$source][] = $target;
-        
-    }
-} else {
-    echo "0 results";
+while ($row = $result->fetch_assoc()) {
+    $data[] = $row;
 }
 
-// Function to create the dot representation of the graph
-function createDotFile($graphData)
-{
-    $dotFileContent = "digraph G {\n";
-    $dotFileContent .= "splines=ortho\n"; // Add this line
-    $dotFileContent .= "node [height=0.1];\n"; // Set a minimum height for nodes
-    foreach ($graphData as $node => $edges) {
-        foreach ($edges as $edge) {
-            $dotFileContent .= "\"$node\" -> \"$edge\" [len=1.6];\n"; // Set len to 2.0 for longer lines
-        }
-    }
-    $dotFileContent .= "}";
-    return $dotFileContent;
-}
-
-// Generate the dot file content
-$dotContent = createDotFile($graphData);
-
-// Save the dot content to a file in the 'assets/images' directory
-file_put_contents('../../assets/images/graph.dot', $dotContent);
-
-// Run Graphviz to generate the diagram and save it to the 'assets/images' directory
-shell_exec('neato -Tpng -Gnodesep=2 -Granksep=2 ../../assets/images/graph.dot -o ../../assets/images/graph.png');
-
-// Output the image to the browser from the 'assets/images' directory
-echo '<div class="graphContainer"><img src="../../assets/images/graph.png" alt="Graph Image"></div>';
-
-// Close the database connection
-$conn->close();
+echo json_encode($data);
 ?>
