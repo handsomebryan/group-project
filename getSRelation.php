4f1eb484edd168ed8073b9db56ea659b24a44e37
Bryan <bryanh365@gmail.com>
2024-04-14 22:30:26 +0800

    Update getSRelation.php

--- getSRelation.php
+++ getSRelation.php
@@ -6,63 +6,33 @@ function getQueryParam($paramName)
     return isset($_GET[$paramName]) ? $_GET[$paramName] : '';
 }
 
+
+
 $id = getQueryParam('id');
 
-// SQL 查詢，計算服務招攬業務員1和招攬業務員2相同序號的保單資料的保險金額總和
-$sqlTop = "SELECT RIGHT(業務員序號, 5) AS 業務員序號, SUM(年化保費) AS 總保費
-        FROM (
-            SELECT 業務員序號, 年化保費
-            FROM 保單資料 a
-            JOIN 服務招攬業務員 b ON a.保單序號 = b.保單序號
-            WHERE RIGHT(b.招攬業務員1序號, 5) LIKE '%$id'
-            UNION ALL
-            SELECT 業務員序號, 年化保費
-            FROM 保單資料 a
-            JOIN 服務招攬業務員 b ON a.保單序號 = b.保單序號
-            WHERE RIGHT(b.招攬業務員2序號, 5) LIKE '%$id'
-        ) AS sales
-        GROUP BY RIGHT(業務員序號, 5)
-        ORDER BY 總保費 DESC
-        LIMIT 5";
 
-$sqlBottom = "SELECT RIGHT(業務員序號, 5) AS 業務員序號, SUM(年化保費) AS 總保費
-        FROM (
-        SELECT 業務員序號, 年化保費
-        FROM 保單資料 a
-        JOIN 服務招攬業務員 b ON a.保單序號 = b.保單序號
-        WHERE RIGHT(b.招攬業務員1序號, 5) LIKE '%$id'
-        UNION ALL
-        SELECT 業務員序號, 年化保費
-        FROM 保單資料 a
-        JOIN 服務招攬業務員 b ON a.保單序號 = b.保單序號
-        WHERE RIGHT(b.招攬業務員2序號, 5) LIKE '%$id'
-    ) AS sales
-    GROUP BY RIGHT(業務員序號, 5)
-    ORDER BY 總保費 ASC
-    LIMIT 5;
-    ";
+$sql = "SELECT RIGHT(招攬業務員1序號, 5) AS 業務員序號, SUM(年化保費) AS 總保費
+FROM 保單資料 a
+JOIN 服務招攬業務員 b ON a.保單序號 = b.保單序號
+WHERE RIGHT(b.業務員序號, 5) LIKE '%$id' AND 招攬業務員1序號
+GROUP BY 招攬業務員1序號
+UNION ALL
+SELECT RIGHT(招攬業務員2序號, 5) AS 業務員序號, SUM(年化保費) AS 總保費
+FROM 保單資料 a
+JOIN 服務招攬業務員 b ON a.保單序號 = b.保單序號
+WHERE RIGHT(b.業務員序號, 5) LIKE '%$id' AND 招攬業務員2序號
+GROUP BY 招攬業務員2序號
+ORDER BY 總保費 DESC
+LIMIT 10;
 
+";
 
+$data = [];
 
-$dataTop = [];
-$dataBottom = [];
-
-$resultTop = $conn->query($sqlTop);
-while ($row = $resultTop->fetch_assoc()) {
-    $dataTop[] = $row;
+$result = $conn->query($sql);
+while ($row = $result->fetch_assoc()) {
+    $data[] = $row;
 }
 
-$resultBottom = $conn->query($sqlBottom);
-while ($row = $resultBottom->fetch_assoc()) {
-    $dataBottom[] = $row;
-}
-
-
-// 將資料返回為 JSON 格式
-$response = [
-    'data' => $dataTop, // 銷售量前五名的資料
-    'data1' => $dataBottom // 銷售量倒數前五名的資料
-];
-
-echo json_encode($response);
+echo json_encode($data);
 ?>
